e069bbcf2b960db08f81a297d0813186b46b7e2f
Bas Biezemans <bas.biezemans@gmail.com>
2026-09-17 09:36:02 +0200

    Add regression test

--- test/test_code.ml
+++ test/test_code.ml
@@ -11,10 +11,14 @@ let test_code_of_string _ =
 let test_code_to_string _ =
   assert_equal "1234" (to_string (D1, D2, D3, D4))
 
+let test_code_to_list _ =
+  assert_equal [D1; D2; D3; D4] (to_list (D1, D2, D3, D4))
+
 let suite =
   "bulls-and-cows" >::: [
     "code_of_string" >:: test_code_of_string;
     "code_to_string" >:: test_code_to_string;
+    "code to list" >:: test_code_to_list;
   ]
 
 let () = run_test_tt_main suite
